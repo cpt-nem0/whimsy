bf2218da02a0701466f3538a68bf834166b85c12
Rohan Yadav <rohanya.76426@gmail.com>
2023-08-10 00:46:30 +0530

    Fixed self review changes | added stdin in the prompt

--- bard_cli/main.py
+++ bard_cli/main.py
@@ -8,25 +8,31 @@ def main():
     cli = CLI()
     args = cli.get_args()
 
-    if args.prompt:
-        prompt = Prompt(
-            args.prompt,
-            options={
-                "system": args.system,
-                "cwd": args.cwd,
-                "cwd_tree": args.cwd_tree,
-            },
-        )
-        cli.print_prompt(prompt.get_prompt())
-        return
+    if not args.prompt:
+        cli.print_error("No prompt provided.")
+        exit(1)
+
+    prompt = Prompt(
+        args.prompt,
+        stdin=cli.get_stdin(),
+        options={
+            "system": args.system,
+            "cwd": args.cwd,
+            "cwd-tree": args.cwd_tree,
+        },
+    )
 
     if args.show:
-        cli.print_prompt(args.question)
+        cli.print_prompt(prompt.get_prompt())
 
     bard_client = BardClient()
-    answer = bard_client.get_answer(args.question)
+    answer = bard_client.get_answer(args.prompt)
     cli.print_answer(answer)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        exit(0)
